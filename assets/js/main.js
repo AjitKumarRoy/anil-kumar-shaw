f30c2b3626ab214c83580459e9246f6100301b90
AjitKumarRoy <ak47riderji@gmail.com>
2024-07-01 18:35:48 +0530

    code-updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,49 @@ AOS.init({
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
 });
+
+
+
+
+
+//close the navbar when the user clicks otuside or on the nav-link
+$(document).ready(function() {
+  // Close navbar when a nav-link is clicked
+  $('.navbar-nav .nav-link').on('click', function() {
+      $('.navbar-collapse').collapse('hide');
+  });
+
+  // Close navbar when clicking outside
+  $(document).click(function(event) {
+      var clickover = $(event.target);
+      var _opened = $(".navbar-collapse").hasClass("show");
+      if (_opened === true && !clickover.hasClass("navbar-toggler")) {
+          $(".navbar-toggler").click();
+      }
+  });
+});
+
+
+
+
+// Get the button
+let moveToTopBtn = document.getElementById("moveToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        moveToTopBtn.style.display = "block";
+    } else {
+        moveToTopBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+moveToTopBtn.addEventListener("click", function() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
